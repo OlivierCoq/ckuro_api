d8c1e30e4d61a01918da0e222f9b02fdc175c6a6
OlivierC <coq.olivier36@gmail.com>
2024-08-08 00:35:03 -0400

    CMS/API Cloudinary bucket connection - Used to - Mutemath

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,37 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  { name: "strapi::logger" },
+  { name: "strapi::errors" },
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "res.cloudinary.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "res.cloudinary.com",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  { name: "strapi::cors" },
+  { name: "strapi::poweredBy" },
+  { name: "strapi::query" },
+  { name: "strapi::body" },
+  { name: "strapi::session" },
+  { name: "strapi::favicon" },
+  { name: "strapi::public" },
 ];
