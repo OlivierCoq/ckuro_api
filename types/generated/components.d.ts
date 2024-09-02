92392fccf59578aa56e154f5ee2636ca4f8bf0ca
OlivierC <coq.olivier36@gmail.com>
2024-09-02 02:07:28 -0400

    These words, Natasha Beddingfield

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,48 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface WorkRatings extends Schema.Component {
+  collectionName: 'components_work_ratings';
+  info: {
+    displayName: 'rating_system';
+    description: '';
+  };
+  attributes: {
+    five_stars: Attribute.Integer & Attribute.Required & Attribute.DefaultTo<0>;
+    four_stars: Attribute.Integer & Attribute.Required & Attribute.DefaultTo<0>;
+    three_stars: Attribute.Integer &
+      Attribute.Required &
+      Attribute.DefaultTo<0>;
+    two_stars: Attribute.Integer & Attribute.Required & Attribute.DefaultTo<0>;
+    one_star: Attribute.Integer & Attribute.Required & Attribute.DefaultTo<0>;
+    average_rating: Attribute.Decimal;
+    num_votes: Attribute.Integer & Attribute.DefaultTo<0>;
+  };
+}
+
+export interface WorkFilter extends Schema.Component {
+  collectionName: 'components_work_filters';
+  info: {
+    displayName: 'filter';
+    description: '';
+  };
+  attributes: {
+    label: Attribute.String;
+    value: Attribute.String;
+    active: Attribute.Boolean & Attribute.DefaultTo<false>;
+  };
+}
+
+export interface WorkArtProjectVideo extends Schema.Component {
+  collectionName: 'components_work_art_project_videos';
+  info: {
+    displayName: 'art_project_video';
+  };
+  attributes: {
+    title: Attribute.String;
+    link: Attribute.String;
+  };
+}
+
 export interface NavigationNavLink extends Schema.Component {
   collectionName: 'components_navigation_nav_links';
   info: {
@@ -33,53 +76,14 @@ export interface CreatorsSocialLink extends Schema.Component {
   };
 }
 
-export interface WorkRatings extends Schema.Component {
-  collectionName: 'components_work_ratings';
-  info: {
-    displayName: 'rating_system';
-    description: '';
-  };
-  attributes: {
-    five_stars: Attribute.Integer;
-    four_stars: Attribute.Integer;
-    three_stars: Attribute.Integer;
-    two_stars: Attribute.Integer;
-    one_star: Attribute.Integer;
-  };
-}
-
-export interface WorkFilter extends Schema.Component {
-  collectionName: 'components_work_filters';
-  info: {
-    displayName: 'filter';
-    description: '';
-  };
-  attributes: {
-    label: Attribute.String;
-    value: Attribute.String;
-    active: Attribute.Boolean & Attribute.DefaultTo<false>;
-  };
-}
-
-export interface WorkArtProjectVideo extends Schema.Component {
-  collectionName: 'components_work_art_project_videos';
-  info: {
-    displayName: 'art_project_video';
-  };
-  attributes: {
-    title: Attribute.String;
-    link: Attribute.String;
-  };
-}
-
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
-      'navigation.nav-link': NavigationNavLink;
-      'creators.social-link': CreatorsSocialLink;
       'work.ratings': WorkRatings;
       'work.filter': WorkFilter;
       'work.art-project-video': WorkArtProjectVideo;
+      'navigation.nav-link': NavigationNavLink;
+      'creators.social-link': CreatorsSocialLink;
     }
   }
 }
