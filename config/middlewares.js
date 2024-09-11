1502263f042704b953a71997d7d6784f28303f8a
OlivierC <coq.olivier36@gmail.com>
2024-09-11 00:27:37 -0400

    Reverting security

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,58 +1,58 @@
 module.exports = [
   { name: "strapi::logger" },
   { name: "strapi::errors" },
-  // {
-  //   name: "strapi::security",
-  //   config: {
-  //     contentSecurityPolicy: {
-  //       useDefaults: true,
-  //       directives: {
-  //         "connect-src": ["'self'", "https:"],
-  //         "img-src": [
-  //           "'self'",
-  //           "data:",
-  //           "blob:",
-  //           "market-assets.strapi.io",
-  //           "res.cloudinary.com",
-  //         ],
-  //         "media-src": [
-  //           "'self'",
-  //           "data:",
-  //           "blob:",
-  //           "market-assets.strapi.io",
-  //           "res.cloudinary.com",
-  //         ],
-  //         upgradeInsecureRequests: null,
-  //       },
-  //     },
-  //   },
-  // },
   {
-    name: 'strapi::security',
+    name: "strapi::security",
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'connect-src': ["'self'", 'https:'],
-          'img-src': [
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
             "'self'",
-            'data:',
-            'blob:',
-            'market-assets.strapi.io',
-            'ckuro-creative-projects.s3.us-east-2.amazonaws.com',
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "res.cloudinary.com",
           ],
-          'media-src': [
+          "media-src": [
             "'self'",
-            'data:',
-            'blob:',
-            'market-assets.strapi.io',
-            'ckuro-creative-projects.s3.us-east-2.amazonaws.com',
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "res.cloudinary.com",
           ],
           upgradeInsecureRequests: null,
         },
       },
     },
   },
+  // {
+  //   name: 'strapi::security',
+  //   config: {
+  //     contentSecurityPolicy: {
+  //       useDefaults: true,
+  //       directives: {
+  //         'connect-src': ["'self'", 'https:'],
+  //         'img-src': [
+  //           "'self'",
+  //           'data:',
+  //           'blob:',
+  //           'market-assets.strapi.io',
+  //           'ckuro-creative-projects.s3.us-east-2.amazonaws.com',
+  //         ],
+  //         'media-src': [
+  //           "'self'",
+  //           'data:',
+  //           'blob:',
+  //           'market-assets.strapi.io',
+  //           'ckuro-creative-projects.s3.us-east-2.amazonaws.com',
+  //         ],
+  //         upgradeInsecureRequests: null,
+  //       },
+  //     },
+  //   },
+  // },
   { name: "strapi::cors" },
   { name: "strapi::poweredBy" },
   { name: "strapi::query" },
