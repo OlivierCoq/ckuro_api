f3fcf2029fd6d49b87a2cc4162b58d377dd36ac9
OlivierC <coq.olivier36@gmail.com>
2024-09-11 00:25:17 -0400

    cors fix additional

--- config/middlewares.js
+++ config/middlewares.js
@@ -58,7 +58,7 @@ module.exports = [
     config: {
       enabled: true,
       headers: '*',
-      origin: ['http://localhost:1337', 'http://localhost:3000', 'http://localhost:3001', 'https://ckuro-creative-projects.s3.us-east-2.amazonaws.com'],
+      origin: ['http://localhost:1337', 'http://localhost:3000', 'http://localhost:3001', 'https://ckuro-creative-projects.s3.us-east-2.amazonaws.com', 'https://ckuro.cc'],
       methods: ['GET', 'POST', 'PUT', 'DELETE'],
       keepHeaderOnError: true,
     }
