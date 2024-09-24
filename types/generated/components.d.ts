2f5e3c5d8c13f7c7a57245d4fb909740c83d7f4a
OlivierC <coq.olivier36@gmail.com>
2024-09-23 23:03:56 -0400

    Adjusting art project schema - Exhaustion, Oli.Vyae

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,38 +1,5 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
-export interface NavigationNavLink extends Schema.Component {
-  collectionName: 'components_navigation_nav_links';
-  info: {
-    displayName: 'nav_link';
-  };
-  attributes: {
-    label: Attribute.String;
-    imagery: Attribute.Media<'images' | 'files' | 'videos' | 'audios', true>;
-    link: Attribute.String;
-  };
-}
-
-export interface CreatorsSocialLink extends Schema.Component {
-  collectionName: 'components_creators_social_links';
-  info: {
-    displayName: 'social_link';
-  };
-  attributes: {
-    platform: Attribute.Enumeration<
-      [
-        'instagram',
-        'facebook',
-        'tiktok',
-        'youtube',
-        'spotify',
-        'soundcloud',
-        'x-twitter'
-      ]
-    >;
-    link: Attribute.String;
-  };
-}
-
 export interface WorkRatings extends Schema.Component {
   collectionName: 'components_work_ratings';
   info: {
@@ -75,17 +42,51 @@ export interface WorkArtProjectVideo extends Schema.Component {
   attributes: {
     title: Attribute.String;
     link: Attribute.String;
+    poster_image: Attribute.Media<'images' | 'files' | 'videos' | 'audios'>;
+  };
+}
+
+export interface NavigationNavLink extends Schema.Component {
+  collectionName: 'components_navigation_nav_links';
+  info: {
+    displayName: 'nav_link';
+  };
+  attributes: {
+    label: Attribute.String;
+    imagery: Attribute.Media<'images' | 'files' | 'videos' | 'audios', true>;
+    link: Attribute.String;
+  };
+}
+
+export interface CreatorsSocialLink extends Schema.Component {
+  collectionName: 'components_creators_social_links';
+  info: {
+    displayName: 'social_link';
+  };
+  attributes: {
+    platform: Attribute.Enumeration<
+      [
+        'instagram',
+        'facebook',
+        'tiktok',
+        'youtube',
+        'spotify',
+        'soundcloud',
+        'x-twitter'
+      ]
+    >;
+    link: Attribute.String;
   };
 }
 
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
-      'navigation.nav-link': NavigationNavLink;
-      'creators.social-link': CreatorsSocialLink;
       'work.ratings': WorkRatings;
       'work.filter': WorkFilter;
       'work.art-project-video': WorkArtProjectVideo;
+      'navigation.nav-link': NavigationNavLink;
+      'creators.social-link': CreatorsSocialLink;
     }
   }
 }
